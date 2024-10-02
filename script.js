f3f07ad2aacbc896492c20add843668f10a85cb4
Samarpannepal <sewa.samarpannepal@gmail.com>
2024-10-01 23:10:40 -0500

    Initial commit for Sewa Samarpan Nepal website

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('apple-pay-button').addEventListener('click', function() {
+    alert('Apple Pay is currently in development.');
+});
+
+document.getElementById('google-pay-button').addEventListener('click', function() {
+    alert('Google Pay is currently in development.');
+});
+
+document.getElementById('card-pay-button').addEventListener('click', function() {
+    alert('Credit/Debit Card payment is currently in development.');
+});
